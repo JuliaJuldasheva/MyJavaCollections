fd517c3199f612420b6fc6475e9a37a43374b2db
JuliaJuldasheva <julia.vergel@gmail.com>
2020-06-26 16:46:42 +0700

    correction after review

--- MyLinkedList/src/main/java/ru/q1w2e3/myLinkedList/MainApp.java
+++ MyLinkedList/src/main/java/ru/q1w2e3/myLinkedList/MainApp.java
@@ -6,6 +6,14 @@ public class MainApp {
         myLinkedList.insertFirst("First");
         myLinkedList.insertFirst("Second");
         myLinkedList.insertFirst("Third");
+        //myLinkedList.insertFirst(null);
+
+        MyLinkedList<Integer> mll = new MyLinkedList<Integer>();
+        mll.insert(0,1);
+        mll.insert(1,2);
+        System.out.println(mll.toString());
+
+       // mll.insert(2,null);
 
         System.out.println(myLinkedList);
         System.out.println(myLinkedList.getSize());
@@ -24,6 +32,19 @@ public class MainApp {
         System.out.println(myLinkedList.getElementByIndex(1));
 
         System.out.println("1 " + myLinkedList.clone());
+
+//        myLinkedList.setLast(null);
+       // myLinkedList.setFirst(null);
+        myLinkedList.insertFirst("3");
+        myLinkedList.insertLast("5");
+        myLinkedList.insert(0,"null");
+        System.out.println(myLinkedList);
+
+//        System.out.println(myLinkedList.getElementByIndex(1));
+        System.out.println(myLinkedList.getElementByIndex(3));
+
+        myLinkedList.insert(1,"10");
+        System.out.println(myLinkedList);
     }
 }
 
